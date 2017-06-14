ea91f36976b639f8b1551cae50a00220403e689b
Oliver Ou <oliver.ou@dji.com>
2017-06-14 16:12:07 +0800

    Add UILibraryDemo project.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Android-UILibraryDemo
-This demo is designed for you to gain a better understanding of the DJI Android UI Library.
+
+## Introduction
+
+This UILibraryDemo is designed for you to gain a basic understanding of the DJI Android UI Library. you will learn how to use DJI Android UI Library and DJI Android SDK to create a fully functioning mini-DJI Go app easily, with standard DJI Go UIs and functionalities. 
+
+## Requirements
+
+ - Android Studio 2.0+
+ - Android System 4.1+
+ - DJI Android SDK 4.1.1
+ - DJI Android UI Library 4.1.1
+
+## Tutorial
+
+For this demo's tutorial: **Getting Started with DJI UI Library**, please refer to <https://developer.dji.com/mobile-sdk/documentation/android-tutorials/UILibraryDemo.html>.
+
+## Feedback
+
+We’d love to hear your feedback on this demo and tutorial.
+
+Please use **Github Issue** or **email** [oliver.ou@dji.com](oliver.ou@dji.com) when you meet any problems of using this demo. At a minimum please let us know:
+
+* Which DJI Product you are using?
+* Which Android Device and Android System version you are using?
+* Which Android Studio version you are using?
+* A short description of your problem includes debugging logs or screenshots.
+* Any bugs or typos you come across.
+
+## License
+
+Android-UILibraryDemo is available under the MIT license. Please see the LICENSE file for more info.
---
+++ UILibraryDemo/app/src/main/java/com/dji/uilibrarydemo/DemoApplication.java
@@ -0,0 +1,131 @@
+package com.dji.uilibrarydemo;
+
+import android.app.Application;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import dji.common.error.DJIError;
+import dji.common.error.DJISDKError;
+import dji.sdk.base.BaseComponent;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.sdkmanager.DJISDKManager;
+
+public class DemoApplication extends Application {
+
+    public static final String FLAG_CONNECTION_CHANGE = "uilibrary_demo_connection_change";
+
+    private static BaseProduct mProduct;
+
+    private Handler mHandler;
+
+    /**
+     * This function is used to get the instance of DJIBaseProduct.
+     * If no product is connected, it returns null.
+     */
+    public static synchronized BaseProduct getProductInstance() {
+        if (null == mProduct) {
+            mProduct = DJISDKManager.getInstance().getProduct();
+        }
+        return mProduct;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mHandler = new Handler(Looper.getMainLooper());
+        //This is used to start SDK services and initiate SDK.
+        DJISDKManager.getInstance().registerApp(this, mDJISDKManagerCallback);
+    }
+
+    /**
+     * When starting SDK services, an instance of interface DJISDKManager.DJISDKManagerCallback will be used to listen to
+     * the SDK Registration result and the product changing.
+     */
+
+    private DJISDKManager.SDKManagerCallback mDJISDKManagerCallback = new DJISDKManager.SDKManagerCallback() {
+
+        //Listens to the SDK registration result
+        @Override
+        public void onRegister(DJIError error) {
+            if(error == DJISDKError.REGISTRATION_SUCCESS) {
+                DJISDKManager.getInstance().startConnectionToProduct();
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Register Success", Toast.LENGTH_LONG).show();
+                    }
+                });
+            } else {
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Register Failed, check network is available", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+            Log.e("TAG", error.toString());
+        }
+
+        //Listens to the connected product changing, including two parts, component changing or product connection changing.
+        @Override
+        public void onProductChange(BaseProduct oldProduct, BaseProduct newProduct) {
+
+            mProduct = newProduct;
+            if(mProduct != null) {
+                mProduct.setBaseProductListener(mDJIBaseProductListener);
+            }
+
+            notifyStatusChange();
+        }
+    };
+
+    private BaseProduct.BaseProductListener mDJIBaseProductListener = new BaseProduct.BaseProductListener() {
+
+        @Override
+        public void onComponentChange(BaseProduct.ComponentKey key, BaseComponent oldComponent, BaseComponent newComponent) {
+
+            if(newComponent != null) {
+                newComponent.setComponentListener(mDJIComponentListener);
+            }
+            notifyStatusChange();
+        }
+
+        @Override
+        public void onConnectivityChange(boolean isConnected) {
+
+            notifyStatusChange();
+        }
+
+    };
+
+    private BaseComponent.ComponentListener mDJIComponentListener = new BaseComponent.ComponentListener() {
+
+        @Override
+        public void onConnectivityChange(boolean isConnected) {
+            notifyStatusChange();
+        }
+
+    };
+
+    private void notifyStatusChange() {
+        mHandler.removeCallbacks(updateRunnable);
+        mHandler.postDelayed(updateRunnable, 500);
+    }
+
+    private Runnable updateRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
+            sendBroadcast(intent);
+        }
+    };
+
+}
